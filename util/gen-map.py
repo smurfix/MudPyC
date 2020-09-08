8f95e9a30e263a348795d0d336ec5cb85a4de8ec
Matthias Urlichs <matthias@urlichs.de>
2020-09-08 17:48:48 +0200

    Fix key grab generation

--- util/gen-map.py
+++ util/gen-map.py
@@ -2,6 +2,13 @@
 
 from itertools import product
 
+# This code generates a set of Mudlet key bindings that translate function
+# and keypad keys with up to three modifiers (Shift, Control, AltGr) to
+# "simple" codes which are then sent to MudPyC.
+# Function keys are numbered starting with 1, keypad digits are 20+digit.
+# the rest of the keypad is 30+.
+# PrintScr can't be grabbed, at least not under Linux, but scroll and pause are.
+
 def key(name,keyid,modid,code,mod):
     return f"""\
             <Key isActive="yes" isFolder="no">
@@ -15,41 +22,41 @@ def key(name,keyid,modid,code,mod):
 """
 
 codes = {
-    "F1":(1,16777264),
-    "F2":(2,16777265),
-    "F3":(3,16777266),
-    "F4":(4,16777267),
-    "F5":(5,16777268),
-    "F6":(6,16777269),
-    "F7":(7,16777270),
-    "F8":(8,16777271),
-    "F9":(9,16777272),
-    "F10":(10,16777273),
-    "F11":(11,16777274),
-    "F12":(12,16777275),
-    "Pause":(15,16777224),
-    "ScrollLock":(14,16777254),
-    "KP 0":(0,48,536870912),
-    "KP 1":(8,49,536870912),
-    "KP 2":(8,50,536870912),
-    "KP 3":(8,51,536870912),
-    "KP 4":(8,52,536870912),
-    "KP 5":(8,53,536870912),
-    "KP 6":(8,54,536870912),
-    "KP 7":(8,55,536870912),
-    "KP 8":(8,56,536870912),
-    "KP 9":(8,57,536870912),
-    "KP *":(8,42,536870912),
-    "KP +":(8,43,536870912),
-    "KP -":(8,45,536870912),
-    "KP /":(8,47,536870912),
-    "KP ,":(8,44,536870912),
-    "KP .":(8,46,536870912),
+    "F1":(1,0x1000030),
+    "F2":(2,0x1000031),
+    "F3":(3,0x1000032),
+    "F4":(4,0x1000033),
+    "F5":(5,0x1000034),
+    "F6":(6,0x1000035),
+    "F7":(7,0x1000036),
+    "F8":(8,0x1000037),
+    "F9":(9,0x1000038),
+    "F10":(10,0x1000039),
+    "F11":(11,0x100003a),
+    "F12":(12,0x100003b),
+    "Pause":(15,0x1000008),
+    "ScrollLock":(14,0x1000026),
+    "KP 0":(20,48,0x20000000),
+    "KP 1":(21,49,0x20000000),
+    "KP 2":(22,50,0x20000000),
+    "KP 3":(23,51,0x20000000),
+    "KP 4":(24,52,0x20000000),
+    "KP 5":(25,53,0x20000000),
+    "KP 6":(28,54,0x20000000),
+    "KP 7":(27,55,0x20000000),
+    "KP 8":(28,56,0x20000000),
+    "KP 9":(29,57,0x20000000),
+    "KP *":(32,42,0x20000000),
+    "KP +":(30,43,0x20000000),
+    "KP -":(31,45,0x20000000),
+    "KP /":(34,47,0x20000000),
+    "KP ,":(35,44,0x20000000),  # you have one or the other
+    "KP .":(35,46,0x20000000),  # so we use the same fn code for both
     }
 mods = {
-    "Shift":(1,33554432),
-    "Control":(2,67108864),
-    "AltGr":(4,1073741824),
+    "AltGr":(4,0x40000000),
+    "Control":(2,0x4000000),
+    "Shift":(1,0x2000000),
 }
 f_mods = ((None,k) for k in mods.keys())
 def all_mods():
